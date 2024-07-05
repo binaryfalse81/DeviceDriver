8ec6a7db14a5e4a3a85a9be10e3dbf0848df4021
JJY <binaryfalse81@gmail.com>
2024-07-05 11:29:39 +0900

    1st commit

--- Project1/DeviceDriver.h
+++ Project1/DeviceDriver.h
@@ -10,4 +10,8 @@ public:
 
 protected:
     FlashMemoryDevice *m_hardware;
+
+private:
+    const int READ_RETRY_CNT = 5;
+    const int ERASE_DATA = 0xFF;
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,54 @@
-#include "pch.h"
-#include "../Project1/DeviceDriver.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../Project1/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+using namespace testing;
+
+class FlashMock : public FlashMemoryDevice
+{
+public:
+    MOCK_METHOD(unsigned char, read, (long address), (override));
+    MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+TEST(DEVICE_DRIVER_TEST, ReadFive)
+{
+    FlashMock mk;
+    DeviceDriver dd{ &mk };
+    EXPECT_CALL(mk, read).Times(5);
+    dd.read(0xA);
+}
+
+TEST(DEVICE_DRIVER_TEST, ReadException)
+{
+    FlashMock mk;
+    DeviceDriver dd{ &mk };
+    EXPECT_CALL(mk, read)
+        .WillOnce(Return(1))
+        .WillOnce(Return(2));
+
+    EXPECT_THROW({ dd.read(0xA); }, exception);
+}
+
+TEST(DEVICE_DRIVER_TEST, Write)
+{
+    FlashMock mk;
+    DeviceDriver dd{ &mk };
+    EXPECT_CALL(mk, read(0xA))
+        .Times(1)
+        .WillRepeatedly(Return(0xFF));
+
+    dd.write(0xA, 10);
+}
+
+TEST(DEVICE_DRIVER_TEST, WriteException)
+{
+    FlashMock mk;
+    DeviceDriver dd{ &mk };
+    EXPECT_CALL(mk, read(0xB))
+        .Times(1)
+        .WillRepeatedly(Return(0xBB));
+
+    EXPECT_THROW({ dd.write(0xB, 10); }, exception);
 }
